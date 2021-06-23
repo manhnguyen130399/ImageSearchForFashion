c65762a1eea7221a539c638d38d2c2bc7ff6e811
Manh Nguyen <manhnguyen130399@gmail.com>
2021-06-23 21:37:44 +0700

    fix

--- detection.py
+++ detection.py
@@ -78,4 +78,4 @@ def  analyzerImage():
         os.remove(file_name)
     return {"data":output}
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(host = '0.0.0.0', port = 5000)
